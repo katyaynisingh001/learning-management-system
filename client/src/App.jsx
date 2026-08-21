225595dc1c85ce7a40cc8b398d457981c04c331c
katyayni24 <katyaynis51@gmail.com>
2026-08-21 13:17:11 +0530

    Add Clerk authentication login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes , useMatch} from 'react-router-dom'
 import AddCourse from './pages/educator/AddCourse'
 import Dashboard from './pages/educator/Dashboard'
 import Educator from './pages/educator/Educator'
@@ -10,22 +10,33 @@ import CoursesList from './pages/student/CoursesList'
 import Home from './pages/student/Home'
 import MyEnrollments from './pages/student/MyEnrollments'
 import Player from './pages/student/Player'
+import Loading from './components/student/Loading'
+import Navbar from './components/student/Navbar'
 
 const App = () => {
+
+  const isEducatorRoute = useMatch('/educator/*');
+
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/course-list' element={<CoursesList />} />
-      <Route path='/course-list/:input' element={<CoursesList />} />
-      <Route path='/course/:id' element={<CourseDetails />} />
-      <Route path='/player/:courseId' element={<Player />} />
-      <Route path='/my-enrollments' element={<MyEnrollments />} />
-      <Route path='/educator' element={<Educator />} />
-      <Route path='/educator/dashboard' element={<Dashboard />} />
-      <Route path='/educator/add-course' element={<AddCourse />} />
-      <Route path='/educator/my-courses' element={<MyCourses />} />
-      <Route path='/educator/students-enrolled' element={<StudentsEnrolled />} />
-    </Routes>
+    <div className='text-default min-h-screen bg-white'>
+      {!isEducatorRoute && <Navbar />}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/course-list' element={<CoursesList />} />
+          <Route path='/course-list/:input' element={<CoursesList />} />
+          <Route path='/course/:id' element={<CourseDetails />} />
+          <Route path='/my-enrollments' element={<MyEnrollments />} />
+          <Route path='/player/:courseId' element={<Player />} />
+          <Route path='loading/:path' element={<Loading />} />
+      
+          <Route path='/educator' element={<Educator />} >
+          <Route path='educator' element={<Dashboard />} />
+          <Route path='add-course' element={<AddCourse />} />
+          <Route path='my-courses' element={<MyCourses />} />
+          <Route path='students-enrolled' element={<StudentsEnrolled />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
+import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const isCourseListPage = location.pathname.includes('/course-list/');
+
+  const{openSignIn} = useClerk()
+  const{user} = useUser()
+
   return (
-    <div>
-        <h1>Navbar</h1>
+    <div className={`flex items-center justify-between px-4 py-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
+      
+      <img src={assets.logo} alt='logo' className='w-28 lg:w-32 cursor-pointer' />
+
+      <div className='hidden items-center gap-5 md:flex text-gray-500'>
+           <div className='flex items-center gap-5'> 
+            { user && 
+            <>
+                  <button>Become an Educator</button>
+                |<Link to='/my-enrollments'>My Enrollments</Link>              
+            </>
+            }
+           </div>
+
+           {user ? <UserButton /> :
+             <button onClick={()=> openSignIn()} className='rounded-full bg-blue-600 px-5 py-2 text-white'>Create Account</button>}
+
+      </div>
+
+      {/* For Phone Screen */}
+      <div className='flex items-center gap-2 md:hidden sm:gap-5 text-gray-500'>
+        <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
+          { user && 
+            <>
+                  <button>Become an Educator</button>
+                |<Link to='/my-enrollments'>My Enrollments</Link>              
+            </>
+            }
+        </div>
+        {
+          user ? <UserButton />
+          : <button onClick={()=> openSignIn()}><img src="{assets.user_icon}" alt="" /></button>
+        }   
+      </div>
+
+      <button className='rounded-full bg-blue-600 px-4 py-2 text-sm font-medium text-white md:hidden'>Menu</button>
     </div>
   )
 }
