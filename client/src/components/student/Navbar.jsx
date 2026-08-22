6946418558e88976e10325ff9ab48db60566a188
katyayni24 <katyaynis51@gmail.com>
2026-08-22 21:32:10 +0530

    Add course rating and views calculation
    Co-authored-by: Nilakshi <diyap0790@gmail.com>

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
 
 const Navbar = () => {
+
+  const {navigate, isEducator} = useContext(AppContext)
 
   const isCourseListPage = location.pathname.includes('/course-list/');
 
@@ -13,13 +16,15 @@ const Navbar = () => {
   return (
     <div className={`flex items-center justify-between px-4 py-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
       
-      <img src={assets.logo} alt='logo' className='w-28 lg:w-32 cursor-pointer' />
+      <img onClick={() => navigate('/')} src={assets.logo} alt='logo' className='w-28 lg:w-32 cursor-pointer' />
 
       <div className='hidden items-center gap-5 md:flex text-gray-500'>
            <div className='flex items-center gap-5'> 
             { user && 
             <>
-                  <button>Become an Educator</button>
+                  <button onClick={() => {navigate('/educator')}}>
+                    {isEducator ? 'Educator Dashboard' : 'Become an Educator'}
+                  </button>
                 |<Link to='/my-enrollments'>My Enrollments</Link>              
             </>
             }
@@ -35,7 +40,9 @@ const Navbar = () => {
         <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
           { user && 
             <>
-                  <button>Become an Educator</button>
+                  <button onClick={() => {navigate('/educator')}}>
+                    {isEducator ? 'Educator Dashboard' : 'Become an Educator'}
+                  </button>
                 |<Link to='/my-enrollments'>My Enrollments</Link>              
             </>
             }
