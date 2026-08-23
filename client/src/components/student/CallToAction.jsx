a08f61fa484090e72b659150748ea9074e5913dc
katyayni24 <katyaynis51@gmail.com>
2026-08-23 14:37:33 +0530

    Add Call To Action Section
    Co-authored-by: Nilakshi <diyap0790@gmail.com>

--- client/src/components/student/CallToAction.jsx
+++ client/src/components/student/CallToAction.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const CallToAction = () => {
   return (
-    <div>
-      <h1>Call to action</h1>
+    <div className='flex flex-col items-center gap-4 pt-10 pb-24 px-8 md:px-0'>
+      <h1 className='text-xl
+      md:text-4xl text-gray-800 font-semibold'>Learn anything, anytime, anywhere</h1>
+      <p className='text-gray-500 sm:text-sm'>Join our community of learners and start your journey today. Explore a wide range of courses and resources designed to help you achieve your goals.</p>
+      <div className='flex items-center font-medium gap-6 mt-4'>
+        <button className='bg-blue-600 text-white px-10 py-3 rounded-md'>Get Started</button>
+        <button className='flex items-center gap-2'>Learn More <img src={assets.arrow_icon} alt="arrow" /> </button>
+      </div>
     </div>
   )
 }
