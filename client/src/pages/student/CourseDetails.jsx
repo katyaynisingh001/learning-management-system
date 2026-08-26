d9c7c83734bbf7981a0aaf674fca740a6cdc8eda
katyayni24 <katyaynis51@gmail.com>
2026-08-26 12:13:26 +0530

    Add the preview video
    Co-authored-by: Nilakshi <diyap0790@gmail.com>

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -5,6 +5,7 @@ import Loading from '../../components/student/Loading'
 import Footer from '../../components/student/Footer'
 import { assets } from '../../assets/assets'
 import humanizeDuration from 'humanize-duration'
+import YouTube from 'react-youtube'
 
 
 const CourseDetails = () => {
@@ -12,8 +13,9 @@ const CourseDetails = () => {
   const [courseData, setCourseData] = useState(null)
   const [openSections, setOpenSections] = useState({})
   const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(false)
+  const [playerData, setPlayerData] = useState(null)
 
-  const { allCourses, calculateRating, calculateCourseDuration, calculateNoOfLectures, calculateChapterTime, currency} = useContext(AppContext)
+  const { allCourses, calculateRating, calculateCourseDuration, calculateNoOfLectures, calculateChapterTime, currency } = useContext(AppContext)
 
   const fetchCourseData = async () => {
     const findCourse = allCourses.find(course => course._id === id)
@@ -24,9 +26,10 @@ const CourseDetails = () => {
     fetchCourseData()
   }, [id, allCourses])
 
-  const toggleSection = (index)=>{
-    setOpenSections((prev)=>(
-      {...prev,
+  const toggleSection = (index) => {
+    setOpenSections((prev) => (
+      {
+        ...prev,
         [index]: !prev[index],
       }
     ));
@@ -62,12 +65,12 @@ const CourseDetails = () => {
             <h2 className='text-xl font-semibold'>Course Duration</h2>
 
             <div className='pt-5'>
-              {courseData.courseContent.map((chapter, index)=> (
+              {courseData.courseContent.map((chapter, index) => (
                 <div key={index} className='border border-gray-300 bg-white mb-2 rounded'>
-                  <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none' onClick={()=> toggleSection(index)}>
+                  <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none' onClick={() => toggleSection(index)}>
                     <div className='flex items-center  gap-2'>
                       <img className={`transform transition-transform ${openSections[index] ? 'rotate-180' : ''}`}
-                      src={assets.down_arrow_icon} alt="arrow icon" />
+                        src={assets.down_arrow_icon} alt="arrow icon" />
                       <p className='font-medium md:text-base text-sm'>{chapter.chapterTitle}</p>
                     </div>
                     <p className='text-sm md:text-default'>
@@ -75,16 +78,20 @@ const CourseDetails = () => {
                     </p>
                   </div>
 
-                  <div className={`overflow-hidden trasition-all duration-300 ${openSections[index] ?'max-h-96' : 'max-h-0'}`}>
+                  <div className={`overflow-hidden trasition-all duration-300 ${openSections[index] ? 'max-h-96' : 'max-h-0'}`}>
                     <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300'>
-                      {chapter.chapterContent.map((lecture, i)=> (
+                      {chapter.chapterContent.map((lecture, i) => (
                         <li key={i} className='flex items-start gap-2 py-1'>
-                          <img src={assets.play_icon} alt="play icon" className='w-4 h-4 mt-1'/>
+                          <img src={assets.play_icon} alt="play icon" className='w-4 h-4 mt-1' />
                           <div className='flex items-center justify-between w-full text-gray-800 text-xs md:text-default'>
                             <p>{lecture.lectureTitle}</p>
                             <div className='flex gap-2'>
-                              {lecture.isPreviewFree && <p className='text-blue-500 cursor-pointer'>Preview</p>}
-                              <p>{humanizeDuration(lecture.lectureDuration * 60 * 1000, {units: ['h', 'm']})}</p>
+                              {lecture.isPreviewFree && <p
+                                onClick={() => setPlayerData({
+                                  videoId: lecture.lectureUrl.split('/').pop()
+                                })}
+                                className='text-blue-500 cursor-pointer'>Preview</p>}
+                              <p>{humanizeDuration(lecture.lectureDuration * 60 * 1000, { units: ['h', 'm'] })}</p>
 
                             </div>
 
@@ -101,67 +108,77 @@ const CourseDetails = () => {
 
           <div className='py-20 text-sm md:text-default'>
             <h3 className='text-xl font-semibold text-gray-800'>Course Description</h3>
-            <p className='pt-3 rich-text' dangerouslySetInnerHTML={{ __html: courseData.courseDescription}}></p>
+            <p className='pt-3 rich-text' dangerouslySetInnerHTML={{ __html: courseData.courseDescription }}></p>
           </div>
 
         </div>
 
         {/* right column */}
         <div className='max-w-course-card z-10 shadow-[0_4px_15px_2px_rgba(0,0,0,0.1)] rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]'>
-          <img src={courseData.courseThumbnail} alt="Course thumbnail" />
+          {
+            playerData ?
+              <YouTube videoId={playerData.videoId} opts={{
+                playerVars: {
+                  autoplay: 1
+                }
+              }} iframeClassName='w-full aspect-video' />
+              :
+              <img src={courseData.courseThumbnail} alt="Course thumbnail" />
+          }
+
           <div className='p-5'>
             <div className='flex items-center gap-2'>
               <img
-        className='w-3.5'
-        src={assets.time_left_clock_icon}
-        alt="time left clock icon"
-      />
+                className='w-3.5'
+                src={assets.time_left_clock_icon}
+                alt="time left clock icon"
+              />
               <p className="text-red-500 text-sm md:text-base">
-  <span className="font-medium">5 days</span> left at this price!
-</p>
+                <span className="font-medium">5 days</span> left at this price!
+              </p>
             </div>
             <div className='flex gap-3 items-center pt-2'>
-            <p className='text-gray-800 md:text-4xl text-2xl font-semibold'>
-              {currency}{(courseData.coursePrice - courseData.discount * courseData.coursePrice / 100).toFixed(2)}
-            </p>
-            <p className='md:text-lg text-gray-500 line-through'>{currency}{courseData.coursePrice}</p>
-            <p className='md:text-lg text-gray-500'>{courseData.discount}% off</p>
-          </div>
-          <div className='flex items-center text-sm md:text-default gap-4 pt-2 md:pt-4 text-gray-500'>
-            <div className='flex items-center gap-1'>
-              <img src={assets.star} alt="star icon" />
-              <p>{calculateRating(courseData)}</p>
+              <p className='text-gray-800 md:text-4xl text-2xl font-semibold'>
+                {currency}{(courseData.coursePrice - courseData.discount * courseData.coursePrice / 100).toFixed(2)}
+              </p>
+              <p className='md:text-lg text-gray-500 line-through'>{currency}{courseData.coursePrice}</p>
+              <p className='md:text-lg text-gray-500'>{courseData.discount}% off</p>
             </div>
-            <div className='h-4 w-px bg-gray-500/40'></div>
-            <div className='flex items-center gap-1'>
-              <img src={assets.time_clock_icon} alt="clock icon" />
-              <p>{calculateCourseDuration(courseData)}</p>
+            <div className='flex items-center text-sm md:text-default gap-4 pt-2 md:pt-4 text-gray-500'>
+              <div className='flex items-center gap-1'>
+                <img src={assets.star} alt="star icon" />
+                <p>{calculateRating(courseData)}</p>
+              </div>
+              <div className='h-4 w-px bg-gray-500/40'></div>
+              <div className='flex items-center gap-1'>
+                <img src={assets.time_clock_icon} alt="clock icon" />
+                <p>{calculateCourseDuration(courseData)}</p>
+              </div>
+
+              <div className='h-4 w-px bg-gray-500/40'></div>
+              <div className='flex items-center gap-1'>
+                <img src={assets.lesson_icon} alt="clock icon" />
+                <p>{calculateNoOfLectures(courseData)} lessons</p>
+              </div>
+            </div>
+            <button className=' md:mt-6 mt-4 w-full py-3 rounded bg-blue-600 text-white font-medium'>{isAlreadyEnrolled ? 'Already Enrolled' : 'Enroll Now'}</button>
+
+            <div className='pt-6'>
+              <p className='md:text-xl text-lg font-medium text-gray-800'>What's in the course?</p>
+              <ul className='ml-4 pt-2 text-sm md:text-default list-disc text-gray-500'>
+                <li>Lifetime access with free updates.</li>
+                <li>Step-by-step, hands-on project guidance.</li>
+                <li>Downloadable resources and source code.</li>
+                <li>Quizzes to test your knowledge.</li>
+                <li>Certificate of completion.</li>
+              </ul>
             </div>
 
-            <div className='h-4 w-px bg-gray-500/40'></div>
-            <div className='flex items-center gap-1'>
-              <img src={assets.lesson_icon} alt="clock icon" />
-              <p>{calculateNoOfLectures(courseData)} lessons</p>
-            </div>
           </div>
-          <button className=' md:mt-6 mt-4 w-full py-3 rounded bg-blue-600 text-white font-medium'>{isAlreadyEnrolled ? 'Already Enrolled' : 'Enroll Now'}</button>
-
-          <div className='pt-6'>
-            <p className='md:text-xl text-lg font-medium text-gray-800'>What's in the course?</p>
-            <ul className='ml-4 pt-2 text-sm md:text-default list-disc text-gray-500'>           
-              <li>Lifetime access with free updates.</li>
-              <li>Step-by-step, hands-on project guidance.</li>
-              <li>Downloadable resources and source code.</li>
-              <li>Quizzes to test your knowledge.</li>
-              <li>Certificate of completion.</li>
-            </ul>
-          </div>
-
         </div>
-      </div>
 
       </div>
-      <Footer/>
+      <Footer />
     </>
   ) : <Loading />
 }
