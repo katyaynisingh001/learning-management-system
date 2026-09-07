c5d522f0ea78e16e5988f8d601e0cfa94f96c398
katyayni24 <katyaynis51@gmail.com>
2026-09-08 00:09:10 +0530

    Add user model and webhook configuration

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
+import { clerkWebhook } from './controllers/webhooks.js'
 
 // Initialize express
 const app = express()
@@ -14,6 +15,7 @@ app.use(cors())
 
 // Routes
 app.get('/', (req, res) => res.send("API Working"))
+app.post('/clerk', express.json(), clerkWebhook)
 
 // Port
 const PORT = process.env.PORT || 5000
