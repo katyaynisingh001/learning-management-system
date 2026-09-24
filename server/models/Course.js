faadfeca551df898f9ab10e8d4fd1e8f7365caed
katyayni24 <katyaynis51@gmail.com>
2026-09-24 07:33:45 +0530

    Created course model

---
+++ server/models/Course.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+
+const lectureSchema = new mongoose.Schema({
+    lectureID: {
+        type: String,required: true
+    },
+    lectureTitle: {
+        type: String,required: true
+    },
+    lectureDuration: {
+        type: Number,required: true
+    },
+    lectureUrl: {
+        type: String,
+        required: true
+    },
+    isPreviewFree: {
+        type: Boolean,
+        required: true
+    },
+    lectureOrder: {
+        type: Number,
+        required: true
+    }
+}, { _id: false
+});
+
+const chapterSchema = new mongoose.Schema({
+    chapterID: {
+        type: String,
+        required: true
+    },
+    chapterOrder: {
+        type: Number,
+        required: true
+    },
+    chapterTitle: {
+        type: String,
+        required: true
+    },
+    chapterContent: [lectureSchema]
+}, { _id: false });
+
+const courseSchema = new mongoose.Schema({
+    courseTitle: {
+        type: String,
+        required: true},
+    courseDescription: {
+        type: String,
+        required: true},
+        courseThumbnail: {
+        type: String,
+        required: true},
+        coursePrice: {
+        type: Number,
+        required: true},
+        isPublished: {
+        type: Boolean,
+        default: true},
+        discount : {
+        type: Number,
+        required: true, min: 0, max: 100},
+        courseContent: [chapterSchema],
+        courseRatings: [{
+            userId: { type : string }, rating: { type : number, min: 1, max: 5 }}],
+            educator : { type : string, ref: 'User' , required: true},
+            enrolledStudents : [{ type : string, ref: 'User'}],},{timestamps: true, minimize: false})
+
+            const Course = mongoose.model('Course', courseSchema);
+
+            export default Course;
